ba791251e9e87d9bb5e30dba6454f338fc3de339
kvirotig <kvirotig@gmail.com>
2021-07-31 01:02:36 +0530

    Update DataExtractAction.java

--- java/dtsf-data-extract/src/main/java/org/ford/data/extract/inbound/DataExtractAction.java
+++ java/dtsf-data-extract/src/main/java/org/ford/data/extract/inbound/DataExtractAction.java
@@ -1,4 +1,5 @@
 public class DataExtractAction {
   public static void main(String[] args) throws Exception {
+       System.out.println("This is Hadoop Job Using Dataproc Cluster *******************");
   }
 }
